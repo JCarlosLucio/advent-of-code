cde0c86b6a9486b9e7933784d50d1784c29fef28
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-12-04 23:12:10 -0600

    Refactor day-1

--- advent-2022/day-1/a.js
+++ advent-2022/day-1/a.js
@@ -1,20 +1,10 @@
 const fs = require('fs');
 const contents = fs.readFileSync('input.txt', 'utf8');
 
-const calories = contents.split('\n'); // ['5474',  '4920', '', '10747', '5855', '13827', '', ...]
+const mostCalories = Math.max(
+  ...contents
+    .split('\n\n')
+    .map((elf) => elf.split('\n').reduce((acc, curr) => acc + Number(curr), 0))
+);
 
-let totalCalories = 0;
-const elfCalories = []; // [123123, 345345, ...]
-
-for (const calorie of calories) {
-  if (calorie === '') {
-    elfCalories.push(totalCalories);
-    totalCalories = 0;
-  } else {
-    totalCalories += Number(calorie);
-  }
-}
-
-const mostCalories = Math.max(...elfCalories);
-
-console.log('Most calories: ', mostCalories);
+console.log('Most calories: ', mostCalories); // Most calories: 66306
